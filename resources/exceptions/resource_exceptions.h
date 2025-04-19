ecdbcb7238fd5dbef26115d4ce28527bf7c0a129
Clair <167580902+ClairRC@users.noreply.github.com>
2025-04-18 20:12:37 -0600

    Delete resources directory

--- resources/exceptions/resource_exceptions.h
+++
@@ -1,7 +0,0 @@
-#ifndef resource_exceptions_h
-#define resource_exceptions_h
-
-#include "FileNotFound.h"
-#include "FailedToLoadShaderException.h"
-
-#endif 
--- resources/resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef resources_h
-#define resrouces_h
-
-#include "header_files/shader.h"
-#include "header_files/vao.h"
-
-#endif 
